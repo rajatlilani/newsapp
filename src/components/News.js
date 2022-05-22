09ab7c21c52391faec8717d16043c0620d39868e
rajatlilani <rajatlilani@gmail.com>
2022-05-22 20:09:59 +0530

    first-commit

---
+++ src/components/News.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Newscomponent from './Newscomponent'
+
+export class News extends Component {
+    render() {
+        return (
+            <div>News
+                <h1><Newscomponent /></h1>
+            </div>
+        )
+    }
+}
+
+export default News
